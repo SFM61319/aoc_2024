8924b3d902f6d41c28bbcb38ca8fd957173b8ce8
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2024-12-03 01:30:05 +0530

    Feat: Solve Day 2 - Part 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -29,6 +29,34 @@ pub fn solve_part1(input: &str) -> usize {
         .count()
 }
 
+fn is_report_safe_with_tolerance(report: &[i64]) -> bool {
+    if is_report_safe(report) {
+        return true;
+    }
+
+    (0..report.len())
+        .into_iter()
+        .map(|ti| {
+            report
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| *i != ti)
+                .map(|(_, &el)| el)
+                .collect::<Vec<_>>()
+        })
+        .map(|report| is_report_safe(&report))
+        .any(|is_safe| is_safe)
+}
+
+#[aoc_runner_derive::aoc(day2, part2)]
+pub fn solve_part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(parse_report)
+        .filter(|report| is_report_safe_with_tolerance(report))
+        .count()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,6 +66,14 @@ mod tests {
         assert_eq!(
             solve_part1("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"),
             2
+        );
+    }
+
+    #[test]
+    fn test_solve_part2_sample() {
+        assert_eq!(
+            solve_part2("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"),
+            4
         );
     }
 }
